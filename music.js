b61d70fceb8651c095eb9f515d8506044035df23
Amit08062001 <amitsingh19052001@gmail.com>
2024-07-20 15:36:22 +0530

    done

---
+++ music.js
@@ -0,0 +1,179 @@
+
+const navItems = document.querySelectorAll(".nav-item");
+
+navItems.forEach((navItem) => {
+  navItem.addEventListener("click", () => {
+    navItems.forEach(item => item.classList.remove("active"));
+    navItem.classList.add("active");
+  });
+});
+
+
+const containers = document.querySelectorAll(".containers");
+
+containers.forEach((container) => {
+  let isDragging = false;
+  let startX;
+  let scrollLeft;
+
+  container.addEventListener("mousedown", (e) => {
+    isDragging = true;
+    startX = e.pageX - container.offsetLeft;
+    scrollLeft = container.scrollLeft;
+  });
+
+  container.addEventListener("mousemove", (e) => {
+    if (!isDragging) return;
+    e.preventDefault();
+    const x = e.pageX - container.offsetLeft;
+    const step = (x - startX) * 0.6;
+    container.scrollLeft = scrollLeft - step;
+  });
+
+  container.addEventListener("mouseup", () => {
+    isDragging = false;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    isDragging = false;
+  });
+});
+
+
+const progress = document.getElementById("progress");
+const song = document.getElementById("song");
+const controlIcon = document.getElementById("controlIcon");
+const playPauseButton = document.querySelector(".play-pause-btn");
+const forwardButton = document.querySelector(".controls .forward");
+const backwardButton = document.querySelector(".controls .backward");
+const rotatingImage = document.getElementById("rotatingImage");
+const songName = document.querySelector(".music-player h2");
+const artistName = document.querySelector(".music-player p");
+
+let rotating = false;
+let currentRotation = 0;
+let rotationInterval;
+
+const songs = [
+  {
+    title: "Kesariya",
+    name: "Arijit Singh",
+    source: "./Assets/Keariya.mp3",
+    cover: "https://i.scdn.co/image/ab67616d0000b273c08202c50371e234d20caf62",
+  },
+  {
+    title: "Apna-Bana-Le",
+    name: "Sachin-Jigar",
+    source: "./Assets/Apna-Bana-Le.mp3",
+    cover: "https://i.ytimg.com/vi/u2NAuswnTKs/maxresdefault.jpg",
+  },
+  {
+    title: "Control",
+    name: "Unknown Brain x Rival",
+    source: "https://github.com/ecemgo/mini-samples-great-tricks/raw/main/song-list/Unknown-BrainxRival-Control.mp3",
+    cover: "https://github.com/ecemgo/mini-samples-great-tricks/assets/13468728/7bd23b84-d9b0-4604-a7e3-872157a37b61",
+  },
+];
+
+let currentSongIndex = 0;
+
+function startRotation() {
+  if (!rotating) {
+    rotating = true;
+    rotationInterval = setInterval(rotateImage, 50);
+  }
+}
+
+function pauseRotation() {
+  clearInterval(rotationInterval);
+  rotating = false;
+}
+
+function rotateImage() {
+  currentRotation += 1;
+  rotatingImage.style.transform = `rotate(${currentRotation}deg)`;
+}
+
+function updateSongInfo() {
+  songName.textContent = songs[currentSongIndex].title;
+  artistName.textContent = songs[currentSongIndex].name;
+  song.src = songs[currentSongIndex].source;
+  rotatingImage.src = songs[currentSongIndex].cover;
+}
+
+song.addEventListener("loadedmetadata", () => {
+  progress.max = song.duration;
+  progress.value = song.currentTime;
+});
+
+song.addEventListener("ended", () => {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+song.addEventListener("timeupdate", () => {
+  if (!song.paused) {
+    progress.value = song.currentTime;
+  }
+});
+
+function playPause() {
+  if (song.paused) {
+    song.play();
+    controlIcon.classList.add("fa-pause");
+    controlIcon.classList.remove("fa-play");
+    startRotation();
+  } else {
+    song.pause();
+    controlIcon.classList.remove("fa-pause");
+    controlIcon.classList.add("fa-play");
+    pauseRotation();
+  }
+}
+
+playPauseButton.addEventListener("click", playPause);
+
+progress.addEventListener("input", () => {
+  song.currentTime = progress.value;
+});
+
+progress.addEventListener("change", () => {
+  song.play();
+  controlIcon.classList.add("fa-pause");
+  controlIcon.classList.remove("fa-play");
+  startRotation();
+});
+
+forwardButton.addEventListener("click", () => {
+  currentSongIndex = (currentSongIndex + 1) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+backwardButton.addEventListener("click", () => {
+  currentSongIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+  updateSongInfo();
+  playPause();
+});
+
+updateSongInfo();
+
+const swiper = new Swiper(".swiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  loop: true,
+  speed: 600,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 10,
+    stretch: 120,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
